20461afbd69bf5b07edc6f5d66e1f89193af578d
m1_air <peterjbishop.denver@gmail.com>
2025-03-21 09:52:01 -0600

    create user and authenticate working

---
+++ swift-scaling-enigma/swift-scaling-enigma/User/SuccessView.swift
@@ -0,0 +1,18 @@
+//
+//  SuccessView.swift
+//  swift-scaling-enigma
+//
+//  Created by Peter Bishop on 3/20/25.
+//
+
+import SwiftUI
+
+struct SuccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuccessView()
+}
